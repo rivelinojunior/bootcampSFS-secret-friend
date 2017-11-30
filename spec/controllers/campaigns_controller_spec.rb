cb9b95e8e29a36b141fa6c737e0cd87f2e22c6b7
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-11-30 02:09:28 -0400

    feat: create test to index campagns_controller

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -1,45 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe CampaignsController, type: :controller do
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
+  # permite o uso dos helpers do devise
+  include Devise::Test::ControllerHelpers
+  
+  # antes de cada teste será feito a autenticação de um 
+  # usuário fake, gerado pelo FactoryGirl
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactoryGirl.create(:user)
+    sign_in @current_user
   end
-
+  
+  # no método index será testado apenas que ele retorna 
+  # o status de sucesso.
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #raffle" do
-    it "returns http success" do
-      get :raffle
       expect(response).to have_http_status(:success)
     end
   end
