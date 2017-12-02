b9672a178f39325adde0dae42e2af420b57b5914
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-02 00:34:44 -0400

    feat: implements method show in campaigns_controller

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,6 +1,8 @@
 class CampaignsController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :set_campaign, only: [:show, :destroy, :update, :raffle]
+  before_action :is_owner?, only: [:show, :destroy, :update, :raffle]
+
   def show
   end
 
@@ -18,4 +20,22 @@ class CampaignsController < ApplicationController
 
   def raffle
   end
+
+  private
+    def set_campaign
+      @campaign = Campaign.find(params[:id])
+    end
+
+    def campaign_params
+      params.require(:campaign).permit(:title, :description).merge(user: current_user)
+    end
+
+    def is_owner?
+      unless current_user == @campaign.user
+        respond_to do |format|
+          format.json { render json: false, status: :forbidden }
+          format.html { redirect_to main_app.root_url }
+        end
+      end
+    end
 end
