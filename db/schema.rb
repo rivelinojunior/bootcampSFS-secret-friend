90c4695a8dba6bfcfdbbbd2658a2c25d95814abf
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-02 11:58:29 -0400

    fix: schema from change column status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129214105) do
+ActiveRecord::Schema.define(version: 20171202133658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20171129214105) do
     t.string   "title"
     t.text     "description"
     t.integer  "user_id"
-    t.integer  "status"
+    t.string   "status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.datetime "event_date"
