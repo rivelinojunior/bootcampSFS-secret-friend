1d5335feaa81a301ce5e1c6c4b71d61e2f1c2599
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-03 19:07:43 -0400

    feat: create job raffle

---
+++ app/jobs/campaign_raffle_job.rb
@@ -0,0 +1,14 @@
+class CampaignRaffleJob < ApplicationJob
+  queue_as :emails
+  
+  def perform(campaign)
+    results = RaffleService.new(campaign).call
+
+    campaign.members.each {|m| m.set_pixel}
+    results.each do |r|
+      CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
+    end
+    campaign.update(status: :finished)
+
+  end
+end
