5e4b34538a51a571622b296b9e6afd95a277e43d
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-12-03 19:38:27 -0400

    reractor: adjust campaing model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,20 +1,19 @@
 class Campaign < ApplicationRecord
+  before_validation :set_member, on: :create
+  before_validation :set_status, on: :create
+
   belongs_to :user
   has_many :members, dependent: :destroy
-  
-  before_create :set_status
-  before_create :set_member
-
   enum status: [:pending, :finished]
+  validates :title, :description, :user, :status, presence: true
 
-  validates :title, :description, :user, presence: true
-  
-  private 
-    def set_status
-      self.status = :pending
-    end
+  private
 
-    def set_member
-      self.members << Member.create(name: self.user.name, email: self.user.email)
-    end
+  def set_status
+    self.status = :pending
+  end
+
+  def set_member
+    self.members << Member.create(name: self.user.name, email: self.user.email)
+  end
 end
