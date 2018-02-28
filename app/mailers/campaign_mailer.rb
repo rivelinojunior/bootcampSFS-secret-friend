dc391b4f5ef0994661f5181f967b38620ee6d15a
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-27 22:35:41 -0400

    create method error_raflle in campign_mailer

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,10 +1,13 @@
 class CampaignMailer < ApplicationMailer
-
   def raffle(campaign, member, friend)
     @campaign = campaign
     @member = member
     @friend = friend
     mail to: @member.email, subject: "Nosso Amigo Secreto: #{@campaign.title}"
   end
-  
+
+  def error_raffle(campaign)
+    @campaign = campaign
+    mail to: @campaign.user.email, subject: "Problemas no sorteio da campanha #{@campaign.title}"
+  end
 end
