932f41c4f0994f371f61cad16e37c5e9a727d93d
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-02-27 22:36:44 -0400

    add rescue in campaign_raffle job

--- app/jobs/campaign_raffle_job.rb
+++ app/jobs/campaign_raffle_job.rb
@@ -1,14 +1,16 @@
 class CampaignRaffleJob < ApplicationJob
   queue_as :emails
-  
+
   def perform(campaign)
-    results = RaffleService.new(campaign).call
-
-    campaign.members.each {|m| m.set_pixel}
-    results.each do |r|
-      CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
+    begin
+      results = RaffleService.new(campaign).call
+      campaign.members.each(&:set_pixel)
+      results.each do |r|
+        CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
+      end
+      campaign.update(status: :finished)
+    rescue
+      CampaignMailer.error_raffle(campaign).deliver_now
     end
-    campaign.update(status: :finished)
-
   end
 end
